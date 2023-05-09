6f066b73bea6393cc0dccb4e38d58758bb878ff9
yafizh <a.chairil.yafizh@gmail.com>
2023-05-10 00:05:31 +0700

    online store navbar with id_pemberi if exsist

--- app/halaman/online_store/partials/navbar.php
+++ app/halaman/online_store/partials/navbar.php
@@ -25,23 +25,18 @@
                     <a class="nav-link <?= $active == 'sale' ? 'text-primary fw-bold' : 'text-dark'; ?>" href="sale.php">Sale</a>
                 </li>
             </ul>
-            <!-- <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            </form> -->
             <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item mx-2 position-relative">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                        3
-                        <span class="visually-hidden">unread messages</span>
-                    </span>
-                    <a class="nav-link text-dark" href="keranjang.php">
+                    <div id="jumlah-keranjang">
+                    </div>
+                    <a class="nav-link <?= $active == 'keranjang' ? 'text-primary fw-bold' : 'text-dark'; ?>" href="keranjang.php">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
                             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                         </svg>
                     </a>
                 </li>
                 <li class="nav-item mx-2">
-                    <a class="nav-link text-dark" href="profile.php">
+                    <a class="nav-link <?= $active == 'profil' ? 'text-primary fw-bold' : 'text-dark'; ?>" href="profil.php">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                         </svg>
@@ -51,3 +46,6 @@
         </div>
     </div>
 </nav>
+<?php if (isset($_SESSION['user']['pembeli'])) : ?>
+    <input type="text" hidden name="id_pembeli" value="<?= $_SESSION['user']['pembeli']['id']; ?>">
+<?php endif; ?>
